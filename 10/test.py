121662dc2a9e4f89842c5883cb4d10854c828bab
WANGWEIRAN <897865273@qq.com>
2019-06-26 17:04:33 +0800

    增加CGI练习

---
+++ 10/test.py
@@ -0,0 +1,9 @@
+# from urllib import request
+#
+#
+# response =request.urlopen('http://www.baidu.com')
+# print(response.read())
+
+from http.server import CGIHTTPRequestHandler,test
+
+test(CGIHTTPRequestHandler)
